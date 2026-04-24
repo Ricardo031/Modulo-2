349f293e0408c7568b3abffe19c9749965296745
Ricardo031 <rjtb31@gmail.com>
2026-04-24 06:35:07 -0500

    semana3: ajustes y mejoras

--- semana3/js/app.js
+++ semana3/js/app.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     const textTest = document.getElementById("text-test");
+    const boton = document.getElementById("toggle-btn");
+    const texto = document.getElementById("hidden-content");
 
-    textTest.addEventListener("click", function() {
+    textTest.addEventListener("click", function () {
         if (textTest.textContent === "Oprimeme") {
             textTest.textContent = "Gracias";
         } else {
             textTest.textContent = "Oprimeme";
         }
     });
+
+    boton.addEventListener("click", function () {
+        texto.classList.toggle('oculto')
+        console.log("hola")
+    })
 });
